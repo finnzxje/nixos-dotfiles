73c2c9f59c40dc6fa00aba2cbf9ca985306b78da
finnzxje <minhthinh0900@gmail.com>
2026-02-04 13:39:53 +0700

    fix: icontheme

--- home/programs/noctalia/default.nix
+++ home/programs/noctalia/default.nix
@@ -8,25 +8,40 @@
   imports = [
     inputs.noctalia.homeModules.default
   ];
+  home.packages = with pkgs; [
+    app2unit # Launch Desktop Entries (or arbitrary commands) as Systemd user units
+    gtk3
+    adwaita-icon-theme # for icon theme
+
+  ];
   programs.noctalia-shell = {
     enable = true;
     systemd.enable = true;
   };
-  # home.packages = [
-  # pkgs.noctalia-shell
-  # pkgs.app2unit # Launch Desktop Entries (or arbitrary commands) as Systemd user units
-  # ];
+
+  gtk = {
+    enable = true;
+
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+  };
+
+  home.sessionVariables = {
+    QT_QPA_PLATFORMTHEME = "gtk3";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+  };
 
   xdg.configFile =
     let
       mkSymlink = config.lib.file.mkOutOfStoreSymlink;
-      confPath = "${config.home.homeDirectory}/nixos-dotfiles/home/programs/noctalia/config";
+      confPath = "${config.home.homeDirectory}/nixos-dotfiles/home/programs/noctalia";
     in
     {
       # NOTE: use config dir as noctalia config because config is not only settings.json
       # https://github.com/noctalia-dev/noctalia-shell/blob/main/nix/home-module.nix#L211-L220
-      "noctalia".source = mkSymlink "${confPath}";
-      # "noctalia".source = ./config;
+      "noctalia".source = mkSymlink "${confPath}/config";
     };
 
 }
