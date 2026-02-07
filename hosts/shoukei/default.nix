7cd34ddc09aee0bbc7028b6e980a267be26377ae
finnzxje <minhthinh0900@gmail.com>
2026-02-07 16:51:21 +0700

    chore: change keybindings, add some system pkgs

--- hosts/shoukei/default.nix
+++ hosts/shoukei/default.nix
@@ -71,6 +71,9 @@
     options = [
       "rw"
       "nofail"
+      "uid=1000"
+      "gid=100"
+      "umask=022"
     ];
   };
 
--- modules/misc.nix
+++ modules/misc.nix
@@ -20,7 +20,11 @@
   services = {
     gvfs.enable = true; # Mount, trash, and other functionalities
     tumbler.enable = true; # Thumbnail support for images
+    samba.enable = true;
+    udisks2.enable = true;
   };
+
+  security.polkit.enable = true;
 
   programs = {
     # dconf is a low-level configuration system.
