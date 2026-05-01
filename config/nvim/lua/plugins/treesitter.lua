4522545e4e83b38d3d17f3443ad5e4dd4ac5c4b8
finnzxje <minhthinh0900@gmail.com>
2026-05-01 23:15:25 +0700

    fix(nvim): update treesitter for neovim 0.12
      Switch nvim-treesitter to main, move highlighting to the new
      vim.treesitter.start() flow, and remove the duplicate old setup from
      autotag.

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,49 +1,101 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  lazy = false,
-  build = ":TSUpdate",
-  opt = {
-    {
-      ignore_install = {},
-      ensure_installed = {
-        "json",
-        "typst",
-        "vimdoc",
-        "go",
-        "rust",
-        "dockerfile",
-        "c",
-        "lua",
-        "python",
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "markdown",
-        "latex",
-        "gitignore",
-        "yaml",
-        "html",
-        "java",
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-      modules = {},
-      sync_install = true,
-      auto_install = true,
+local fallback_parsers = {
+	"bash",
+	"c",
+	"css",
+	"diff",
+	"dockerfile",
+	"gitignore",
+	"go",
+	"gomod",
+	"html",
+	"java",
+	"javascript",
+	"jsdoc",
+	"json",
+	"latex",
+	"lua",
+	"luadoc",
+	"markdown",
+	"markdown_inline",
+	"nix",
+	"odin",
+	"python",
+	"query",
+	"rust",
+	"tsx",
+	"typst",
+	"typescript",
+	"vim",
+	"vimdoc",
+	"yaml",
+}
 
-      incremental_selection = {
-        enable = false,
-        keymaps = {
-          init_selection = "gnn", -- set to `false` to disable one of the mappings
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
-        },
-      },
-    },
-  },
+local enabled_filetypes = {
+	"bash",
+	"c",
+	"css",
+	"diff",
+	"dockerfile",
+	"gitignore",
+	"go",
+	"gomod",
+	"html",
+	"java",
+	"javascript",
+	"json",
+	"latex",
+	"lua",
+	"markdown",
+	"nix",
+	"odin",
+	"python",
+	"query",
+	"rust",
+	"tsx",
+	"typst",
+	"typescript",
+	"vim",
+	"vimdoc",
+	"yaml",
+}
+
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local ok, treesitter = pcall(require, "nvim-treesitter")
+
+		if ok and type(treesitter.install) == "function" then
+			treesitter.setup()
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = enabled_filetypes,
+				callback = function(args)
+					local started = pcall(vim.treesitter.start, args.buf)
+					if started then
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
+
+			return
+		end
+
+		local configs_ok, configs = pcall(require, "nvim-treesitter.configs")
+		if not configs_ok then
+			return
+		end
+
+		configs.setup({
+			ensure_installed = fallback_parsers,
+			auto_install = true,
+			highlight = { enable = vim.fn.has("nvim-0.12") == 0 },
+			indent = { enable = vim.fn.has("nvim-0.12") == 0 },
+			modules = {},
+			sync_install = false,
+			ignore_install = {},
+		})
+	end,
 }
